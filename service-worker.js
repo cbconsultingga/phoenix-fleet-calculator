72a5b0f85fae3d6e391b4613f75c5d4a30853514
cbconsultingga <cb.consulting.ga@gmail.com>
2026-05-27 10:12:24 -0400

    Fix service worker cache list

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'phoenix-fleet-calculator-v1';
-const FILES_TO_CACHE = ['./index.html', './manifest.json', './icons/icon.svg'];
+const FILES_TO_CACHE = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
